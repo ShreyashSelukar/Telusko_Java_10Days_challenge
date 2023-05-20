107fd8bc976dbe0f63a89199d1467e0084f10add
ShreyashSelukar <106154810+ShreyashSelukar@users.noreply.github.com>
2023-05-20 21:21:35 +0530

    Update Pascal_Triangle_Memoization.java

--- Day_1_Pascal_Triangle/Pascal_Triangle_Memoization.java
+++ Day_1_Pascal_Triangle/Pascal_Triangle_Memoization.java
@@ -3,14 +3,14 @@ import java.util.HashMap;
 public class Pascal_Triangle_Memoization {
     public static void main(String[] args){
         int n =5;
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String,Integer> cache_map = new HashMap<>();
         for(int row =0; row<n;row++){
             for(int column=0;column<=row;column++){
-                if(map.containsKey((String.format("%s%s",row,column)))){
-                    System.out.print(map.get((String.format("%s%s",row,column))));
+                if(cache_map.containsKey((String.format("%s%s",row,column)))){
+                    System.out.print(cache_map.get((String.format("%s%s",row,column))));
                 }else{
-                    map.put(String.format("%s%s",row,column),PascalTriangle(row,column));
-                    System.out.print(map.get((String.format("%s%s",row,column))));
+                    cache_map.put(String.format("%s%s",row,column),PascalTriangle(row,column));
+                    System.out.print(cache_map.get((String.format("%s%s",row,column))));
                 }
             }
             System.out.println();
