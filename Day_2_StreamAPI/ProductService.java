449a137739fb94c60006e49745ecea18084b9a24
ShreyashSelukar <106154810+ShreyashSelukar@users.noreply.github.com>
2023-05-21 18:59:45 +0530

    Update ProductService.java

--- Day_2_StreamAPI/ProductService.java
+++ Day_2_StreamAPI/ProductService.java
@@ -14,13 +14,18 @@ public class ProductService {
     public List<Product> getAllProducts(){
         return products;
     }
-
+    
+    /** This method will search the product according to given exact key word as place. 
+    Also it will handle the lowercase scenarios of place name.    **/
     public Product getProductByPlace(String place){
         return products.stream()
                 .filter(p -> (p.getPlace().toLowerCase().equals(place.toLowerCase())))
                 .findFirst()
                 .orElse(null);
     }
+    
+    /** This method will return type as List but we are using stream to filter out of warranty products. 
+    And converting Stream to List using collect(Collectors.toList()).    **/    
     public List<Product> getOutOfWarrantyProduct(int currentYear) {
             return products.stream()
                     .filter(p -> (p.getWarranty() < currentYear))
