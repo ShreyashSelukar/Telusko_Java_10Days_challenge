09dc4497c6a161c8822383118ebea3025a4363cf
ShreyashSelukar <106154810+ShreyashSelukar@users.noreply.github.com>
2023-05-23 00:22:26 +0530

    Update ProductDB.java

--- Day_3_Postgres_JPA_SpringBoot/src/main/java/com/selukar/ProductSpring/ProductDB.java
+++ Day_3_Postgres_JPA_SpringBoot/src/main/java/com/selukar/ProductSpring/ProductDB.java
@@ -1,4 +1,4 @@
-package com.prajakta.ProductSpring;
+package com.selukar.ProductSpring;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
