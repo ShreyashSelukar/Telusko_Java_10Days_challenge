dcc0ed7d5420949216def6a1b9cf90ba1c72fa26
ShreyashSelukar <106154810+ShreyashSelukar@users.noreply.github.com>
2023-05-22 22:25:32 +0530

    Add files via upload

---
+++ Day_3_Postgres_JPA_SpringBoot/src/main/java/com/selukar/ProductSpring/ProductService.java
@@ -0,0 +1,26 @@
+package com.selukar.ProductSpring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    // it'll provide object of productDb automatically
+    @Autowired
+    ProductDB db;
+    public List<Product> getAllProducts(){
+        return db.findAll();
+    }
+    public List<Product> getProductByName(String name){
+        return db.findByNameIgnoreCase(name.toLowerCase());
+    }
+    public List<Product> getProductsByText(String searchText){
+        return db.findAllByAnyColumnContaining(searchText.toLowerCase());
+    }
+    public List<Product> getOutOfWarranty(Integer warranty){
+        return db.findByWarrantyLessThan(warranty);
+    }
+
+}
